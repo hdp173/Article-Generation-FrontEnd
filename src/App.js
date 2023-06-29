ff0fb75ee0487d3aead3f473ddc2f704a44cd6b8
EntornoJT <vkurylo417@gmail.com>
2023-06-29 11:34:04 +0100

    UI for prompt selecting, editing and training data

--- src/App.js
+++ src/App.js
@@ -1,64 +1,121 @@
 import "./App.css";
 import { Form, FormGroup, Label, Input, Button, Col } from "reactstrap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const baseUrl = "http://95.164.44.248:8080";
+
 function App() {
-  const [article, setArticle] = useState("");
-  const [isGenerating, setIsGenerating] = useState(false);
-  const [topic, setTopic] = useState("");
-  const handleClick = () => {
-    setIsGenerating(true);
-    fetch("http://95.164.44.248:8080/generate-article", {
+  const [trainFile, setTrainFile] = useState(null);
+  const [indexes, setIndexes] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState("");
+  const [currentPrompt, setCurrentPrompt] = useState("");
+
+  const fetchPrompt = (index) => {
+    const formData = new FormData();
+    formData.append("index", index);
+    fetch(`${baseUrl}/get-prompt`, {
       method: "POST",
-      body: JSON.stringify({ query: topic }),
-      headers: {
-        "Content-Type": "application/json",
-      },
+      body: formData,
     })
-      .then(async (response) => {
-        setIsGenerating(false);
-        const result = await response.json();
-        console.log(result);
-        setArticle(result);
-      })
-      .catch((err) => {
-        console.error(err);
-        setIsGenerating(false);
-      });
+      .then((response) => response.json())
+      .then((result) => setCurrentPrompt(result));
   };
+  const uploadTrainFile = () => {
+    const formData = new FormData();
+    formData.append("file", trainFile);
+    formData.append("index", currentIndex);
+    fetch(`${baseUrl}/train-data`, {
+      method: "POST",
+      body: formData,
+    }).then(async (response) => {
+      const result = await response.json();
+      alert("Succeed");
+      console.log(result);
+    });
+  };
+  const handleIndexChange = (e) => {
+    setCurrentIndex(e.target.value);
+    fetchPrompt(e.target.value);
+  };
+
+  const savePrompt = () => {
+    const formData = new FormData();
+    formData.append("index", currentIndex);
+    formData.append("prompt", currentPrompt);
+    fetch(`${baseUrl}/save-prompt`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then(() => alert("Succed"));
+  };
+  useEffect(() => {
+    fetch(`${baseUrl}/get-all-prompts`)
+      .then((response) => response.json())
+      .then((data) => {
+        setIndexes(data);
+        fetchPrompt(data[0]);
+      });
+  }, []);
   return (
     <div>
       <main className="mt-5">
         <Form>
           <FormGroup row>
-            <Label sm={2} for="topic"></Label>
-            <Col sm={8}>
+            <Label sm={1} for="train_file" style={{ textAlign: "right" }}>
+              Select Index
+            </Label>
+            <Col sm={6}>
               <Input
-                id="topic"
-                name="topic"
-                placeholder="input topic"
-                onChange={(e) => setTopic(e.target.value)}
-              />
-            </Col>
-            <Col sm={2}>
-              <Button onClick={handleClick} disabled={isGenerating}>
-                Generate
-              </Button>
+                type="select"
+                name="index"
+                id="index"
+                onChange={handleIndexChange}
+              >
+                {indexes.map((index) => (
+                  <option value={index}>{index}</option>
+                ))}
+              </Input>
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label sm={2} for="article"></Label>
-            <Col sm={8}>
+            <Label sm={1} for="train_file" style={{ textAlign: "right" }}>
+              Prompt
+            </Label>
+            <Col sm={6}>
               <textarea
-                id="article"
-                name="article"
-                readOnly
-                value={article}
+                name="prompt"
+                id="prompt"
                 style={{ width: "100%", height: "500px" }}
+                value={currentPrompt}
+                onChange={(e) => setCurrentPrompt(e.target.value)}
+              ></textarea>
+              <Col sm={2}>
+                <Button onClick={savePrompt} className="btn-primary">
+                  Save
+                </Button>
+              </Col>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label sm={1} for="train_file" style={{ textAlign: "right" }}>
+              Choose Train File
+            </Label>
+            <Col sm={6}>
+              <Input
+                id="train_file"
+                name="train_file"
+                type="file"
+                onChange={(e) => setTrainFile(e.target.files[0])}
               />
             </Col>
+            {trainFile && (
+              <Col sm={2}>
+                <Button onClick={uploadTrainFile}>Upload Train Data</Button>
+              </Col>
+            )}
           </FormGroup>
         </Form>
       </main>
